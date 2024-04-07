e659a9f3711cac667ada339682cd10cd53431007
Walter Steur <stewartrule@gmail.com>
2024-04-07 20:27:45 +0200

    Initial Commit

---
+++ FoodiesIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodiesIOS
+//
+//  Created by Walter Steur on 07/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodiesIOS/FoodiesIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodiesIOSApp.swift
+//  FoodiesIOS
+//
+//  Created by Walter Steur on 07/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodiesIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
